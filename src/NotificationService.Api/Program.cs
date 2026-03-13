2ab24f47840de70dc003b0927c5459e00845ac35
manojkumar <manojkumarmeda@gmail.com>
2026-03-13 21:05:51 +0530

    Enable Swagger UI middleware for all environments

--- src/NotificationService.Api/Program.cs
+++ src/NotificationService.Api/Program.cs
@@ -1,4 +1,4 @@
-﻿using NotificationService.Repository;
+using NotificationService.Repository;
 using NotificationService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,11 +14,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -27,6 +24,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
 
 
 
